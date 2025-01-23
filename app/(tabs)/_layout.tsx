8e82e01ef453f1578fee1ad9d917185001d66287
Kaushik Saha <kaushiksaha004@gmail.com>
2025-01-24 01:57:21 +0530

    add wallets tab and remove scanner tab; create Welcome component

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,4 +1,3 @@
-import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { Tabs } from 'expo-router'
 import AntDesign from '@expo/vector-icons/AntDesign';
@@ -8,13 +7,13 @@ import Ionicons from '@expo/vector-icons/Ionicons';
 export default function _layout() {
   return (
     <Tabs>
+      <Tabs.Screen name="wallets" options={{
+        title: 'Wallets',
+        tabBarIcon: ({ color }) => <MaterialCommunityIcons name="qrcode-scan" size={24} color={color} />
+      }} />
       <Tabs.Screen name='index' options={{
         title: 'Home',
         tabBarIcon: ({ color }) => <AntDesign name="home" size={24} color={color} />
-      }} />
-      <Tabs.Screen name="scanner" options={{
-        title: 'Scanner',
-        tabBarIcon: ({ color }) => <MaterialCommunityIcons name="qrcode-scan" size={24} color={color} />
       }} />
       <Tabs.Screen name="settings" options={{
         title: 'Settings',
